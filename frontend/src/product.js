f69dea80a05046ca938ca961b72f0aae43d5f5f5
Kat Contreras <kat.contreras@gmail.com>
2023-01-26 23:24:30 -0800

    added products

---
+++ frontend/src/product.js
@@ -0,0 +1,69 @@
+const products = [
+    {
+        _id: '1',
+        name: 'A.Au',
+        image: '/images/products/product-1.jpeg',
+        description: 'yayayayayayya',
+        brand: '',
+        category: 'Womenswear',
+        price: 100,
+        countInStock: 2,
+        rating: 4.5,
+        numReviews: 5,
+
+    },
+    {
+        _id: '2',
+        name: 'aakofii',
+        image: '/images/products/product-2.jpeg',
+        description: 'yayayayayayya',
+        brand: '',
+        category: 'Menswear',
+        price: 100,
+        countInStock: 2,
+        rating: 4.5,
+        numReviews: 5,
+
+    },
+    {
+        _id: '3',
+        name: 'AAKS',
+        image: '/images/products/product-3.jpeg',
+        description: 'yayayayayayya',
+        brand: '',
+        category: 'Menswear',
+        price: 100,
+        countInStock: 2,
+        rating: 4.5,
+        numReviews: 5,
+
+    },
+    {
+        _id: '4',
+        name: 'Ace kouture',
+        image: '/images/products/product-4.jpeg',
+        description: 'yayayayayayya',
+        brand: '',
+        category: 'Menswear',
+        price: 100,
+        countInStock: 2,
+        rating: 4.5,
+        numReviews: 5,
+
+    },
+    {
+        _id: '5',
+        name: 'Adha Zelma',
+        image: '/images/products/product-5.jpeg',
+        description: 'yayayayayayya',
+        brand: '',
+        category: 'Menswear',
+        price: 100,
+        countInStock: 2,
+        rating: 4.5,
+        numReviews: 5,
+
+    }
+]
+
+export default products
