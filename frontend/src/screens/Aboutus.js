28c197f70a112ff16ba7ae52eacba1a9f319e292
Kat Contreras <kat.contreras@gmail.com>
2023-02-01 15:57:19 -0800

    updated products

--- frontend/src/screens/Aboutus.js
+++ frontend/src/screens/Aboutus.js
@@ -1,6 +1,9 @@
 import React from 'react'
-import { Container, Button } from 'react-bootstrap'
+import { Container, Button, Row, Col } from 'react-bootstrap'
 import Slide from '../components/Slide';
+import Product from '../components/Product'
+import products from '../products'
+
 
 
 const Aboutus = () => {
@@ -8,7 +11,14 @@ const Aboutus = () => {
     <Container id='about'>
     <Slide />
     <div className='container-fluid'>
-    
+    <h4>Latest Products</h4>
+    <Row>
+      {products.map(product => (
+        <Col sm={12} md={6} lg={4} xl={3} >
+            <Product product={product} />
+        </Col>
+      ))}
+    </Row>
     </div>
     
 
