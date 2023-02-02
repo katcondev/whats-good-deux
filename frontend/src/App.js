4292dddfa0f8593921fd30f8efc7be68145fa2d3
Kat Contreras <kat.contreras@gmail.com>
2023-02-01 17:47:38 -0800

    updated images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import About from "./screens/Aboutus";
+import About from "./screens/Home";
 
 import {
   BrowserRouter as Router,
---
+++ frontend/src/screens/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Slide from "../components/Slide";
+import Product from "../components/Product";
+import products from "../products";
+
+const Aboutus = () => {
+  return (
+    <Container id='about'>
+      <Slide />
+      <div className='mx-auto py-3 container'>
+        <h3>Popular Drinks</h3>
+        <Row>
+          {products.map((product) => (
+            <Col sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      </div>
+    </Container>
+  );
+};
+
+export default Aboutus;
