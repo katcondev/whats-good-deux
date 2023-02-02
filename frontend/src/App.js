968074920f2b2f724306980577ded1f7f76f2a8b
Kat Contreras <kat.contreras@gmail.com>
2023-02-01 16:20:22 -0800

    updated files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,30 @@
-import React from 'react'
-import Header from './components/Header';
-import Footer from './components/Footer';
-import About from './screens/Aboutus';
-import Reviews from './screens/Reviews';
-import Cart from './screens/Cart'
-import Login from './screens/Login'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import About from "./screens/Aboutus";
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <Router>
-      <Header />
-      <main>
-      <Routes>
-        <Route path='/' element={<About />} />
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/login' element={<Login />} />
-        <Route path="*" element={<Navigate replace to="/" />} />
-      </Routes>
-      
-      </main>
-      <Footer />
+        <Header />
+        <main>
+          <Routes>
+            <Route path='/' element={<About />} />
+            <Route path='*' element={<Navigate replace to='/' />} />
+          </Routes>
+        </main>
+        <Footer />
       </Router>
     </div>
   );
-}
+};
 
 export default App;
--- frontend/src/components/Slide.js
+++ frontend/src/components/Slide.js
@@ -1,40 +1,44 @@
-import React, { useState } from 'react'
-import { Carousel } from 'react-bootstrap'
+import React, { useState } from "react";
+import { Carousel } from "react-bootstrap";
 
 const Slide = () => {
-    const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(0);
 
-    const handleSelect = (selectedIndex, e) => {
-      setIndex(selectedIndex);
-    }
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
 
   return (
     <div className='container w-100'>
-      <Carousel activeIndex={index} onSelect={handleSelect} className="container-fluid">
+      <Carousel
+        activeIndex={index}
+        onSelect={handleSelect}
+        className='container-fluid'
+      >
         <Carousel.Item>
           <img
-            className="d-block w-100"
-            src="/images/slide-1.png"
-            alt="First slide"
+            className='d-block w-100'
+            src='/images/slide-1.png'
+            alt='First slide'
           />
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="d-block w-100"
-            src="/images/slide-2.png"
-            alt="Second slide"
+            className='d-block w-100'
+            src='/images/slide-2.png'
+            alt='Second slide'
           />
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="d-block w-100"
-            src="/images/slide-3.png"
-            alt="Third slide"
+            className='d-block w-100'
+            src='/images/slide-3.png'
+            alt='Third slide'
           />
         </Carousel.Item>
       </Carousel>
     </div>
-  )
-}
+  );
+};
 
-export default Slide
+export default Slide;
