f7f77d170aacc4af0bbef6a48e1d2e9ee1ac73ee
katcontrerasdev <kat.contreras@gmail.com>
2023-02-08 16:48:31 -0800

    prod

--- backend/server.js
+++ backend/server.js
@@ -1,59 +1,19 @@
-require("dotenv").config();
 const express = require("express");
-const fs = require("fs");
+const products = require("./data/products");
 
-const PORT = process.env.PORT || 5015;
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// get data from products.json
-app.get("/products", (req, res) => {
-  fs.readFile(__dirname + "/data/products.json", "utf8", (err, data) => {
-    if (err) {
-      return console.log(err);
-    }
-    console.log("These are Products", data);
-    res.json(JSON.parse(data));
-  });
+app.get("/", (req, res) => {
+  res.send("Api is running");
 });
 
-// post data to products.json
-app.post("/products", (req, res) => {
-  fs.readFile(__dirname + "/data/products.json", "utf8", (err, data) => {
-    if (err) {
-      return console.log(err);
-    }
-    products = JSON.parse(data);
-
-    let id = products[products.length - 1].id + 1;
-    let newProduct = {
-      nameP: req.body.nameP,
-      image: req.body.image,
-      description: req.body.description,
-      brand: req.body.brand,
-      category: req.body.category,
-      price: req.body.price,
-      countInStock: req.body.countInStock,
-      rating: req.body.rating,
-      numReviews: req.body.numReviews,
-      _id: id,
-    };
-    let activeProduct = products.concat(newProduct);
-
-    fs.writeFile(
-      __dirname + "/data/products.json",
-      JSON.stringify(activeProduct),
-      (err, data) => {
-        if (err) {
-          return err;
-        }
-        console.log(activeProduct);
-        res.json(activeProduct);
-      }
-    );
-  });
+app.get("/api/products", (req, res) => {
+  res.json(products);
 });
 
-app.listen(PORT, console.log(`Server running on port ${PORT}`));
+app.get("/api/product/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(5001, console.log(`Server running on port 5001`));
--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -9,12 +9,11 @@ const Aboutus = () => {
     <>
       <Slide />
       <Container id='about'>
-      
         <div className='mx-auto py-3 container'>
           <h3 className='pt-3'>Popular Drinks</h3>
           <Row>
             {products.map((product) => (
-              <Col sm={12} md={6} lg={4} xl={3}>
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                 <Product product={product} />
               </Col>
             ))}
