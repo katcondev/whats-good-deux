67d630a08ecbfc6a3638e56e63f1905ff1bb50a6
katcontrerasdev <kat.contreras@gmail.com>
2023-02-23 00:20:40 -0800

    Updated files

--- backend/server.js
+++ backend/server.js
@@ -16,12 +16,17 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 
-app.use(express.static("frontend/build"));
+// app.use(express.static("frontend/build"));
 
-// Express serve up index.html file if it doesn't recognize route
+// // Express serve up index.html file if it doesn't recognize route
 
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "backend", "build", "index.html"));
+// });
+
+app.use(express.static(path.join(__dirname, "frontend/build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "backend", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend/build"));
 });
 
 process.on("unhandledRejection", (err) => {
@@ -30,5 +35,5 @@ process.on("unhandledRejection", (err) => {
   console.log("____________");
 });
 
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running on port 5002`));
