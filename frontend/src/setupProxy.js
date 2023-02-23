722d4f34c647f93ef9ce41137324f7cf6cac2478
katcontrerasdev <kat.contreras@gmail.com>
2023-02-23 02:17:57 -0800

    Updated files

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,9 +2,9 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "/api",
+    "/api/products",
     createProxyMiddleware({
-      target: "http://localhost:5006/api/products",
+      target: "http://localhost:5006",
       changeOrigin: true,
     })
   );
